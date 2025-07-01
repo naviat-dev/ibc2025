95471c86a54b5f51e83bf8331649eed0a113c96e
Israel Emmanuel <sriemmanuel787@outlook.com>
2025-07-01 11:25:04 -0600

    Adds slide animation and improves page layout
    Enhances the MainPage layout by introducing a root grid with
    transform capabilities. Implements a slide animation to
    smoothly transition between modes, improving user experience.
    Refactors navigation logic to utilize storyboard animations.

--- ibc2025/MainPage.xaml.cs
+++ ibc2025/MainPage.xaml.cs
@@ -1,3 +1,5 @@
+using Microsoft.UI.Xaml.Media.Animation;
+
 namespace ibc2025;
 
 public sealed partial class MainPage : Page
@@ -6,17 +8,68 @@ public sealed partial class MainPage : Page
     {
         InitializeComponent();
         PageBackground.Background = App.DailyBackground;
+
     }
 
     private void StartMaster(object sender, RoutedEventArgs e)
     {
         App.MASTER_MODE = true;
-        _ = ((Frame)Window.Current.Content).Navigate(typeof(QuestionBoardPage));
+        Storyboard storyboard = SlideAnimation("X", TimeSpan.FromSeconds(0.5));
+        storyboard.Completed += (s, args) =>
+        {
+            _ = ((Frame)Window.Current.Content).Navigate(typeof(QuestionBoardPage));
+        };
+        storyboard.Begin();
     }
 
     private void StartMirror(object sender, RoutedEventArgs e)
     {
         App.MASTER_MODE = false;
-        _ = ((Frame)Window.Current.Content).Navigate(typeof(QuestionBoardPage));
+        Storyboard storyboard = SlideAnimation("X", TimeSpan.FromSeconds(0.5));
+        storyboard.Completed += (s, args) =>
+        {
+            _ = ((Frame)Window.Current.Content).Navigate(typeof(QuestionBoardPage));
+        };
+        storyboard.Begin();
+    }
+
+    private Storyboard SlideAnimation(string axis, TimeSpan duration, int offset = -200)
+    {
+        Storyboard storyboard = new();
+
+        DoubleAnimation animationPos = new()
+        {
+            To = -200,
+            Duration = TimeSpan.FromSeconds(0.5),
+            EasingFunction = new ExponentialEase
+            {
+                EasingMode = EasingMode.EaseOut,
+                Exponent = 5
+            }
+        };
+
+        DoubleAnimation animationOpacity = new()
+        {
+            To = 0.0, // Fade out to invisible
+            Duration = TimeSpan.FromSeconds(0.5),
+            EasingFunction = new ExponentialEase
+            {
+                EasingMode = EasingMode.EaseOut,
+                Exponent = 5
+            }
+        };
+
+        Storyboard.SetTarget(animationOpacity, RootGrid);
+        Storyboard.SetTargetProperty(animationOpacity, "Opacity");
+
+        storyboard.Children.Add(animationOpacity);
+
+        Storyboard.SetTarget(animationPos, MainTransform);
+        Storyboard.SetTargetProperty(animationPos, axis);
+
+        storyboard.Children.Add(animationPos);
+        storyboard.Begin();
+
+        return storyboard;
     }
 }
